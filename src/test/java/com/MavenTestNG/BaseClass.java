f468490570762eda87d9ea6451a8c6f57ca01a46
wuikhan <wuikhan@gmail.com>
2018-12-29 12:24:48 -0500

    this push will fail

--- src/test/java/com/MavenTestNG/BaseClass.java
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -1,5 +1,6 @@
 package com.MavenTestNG;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,5 +12,10 @@ public class BaseClass {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver2");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void failThis() {
+		Assert.assertEquals(12, 13);
 	}
 }
