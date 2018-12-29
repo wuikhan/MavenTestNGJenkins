157adcc34bd9c2864712c72adf7e0f6ae08a56d7
wuikhan <wuikhan@gmail.com>
2018-12-29 11:12:25 -0500

    initial commit

---
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -0,0 +1,15 @@
+package com.MavenTestNG;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	@Test
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver2");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+}
